b64070926ad7a40a6de3469daf862aa87793ffd5
Jan Gertgens <external.jan.gertgens@bosch-si.com>
2015-10-29 07:28:18 +0100

    helloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ext.gej1wa3 on 29.10.2015.
+ */
+public class HelloWorld {
+    public static void main (String[] args)
+    {
+        // Ausgabe Hello World!
+        System.out.println("Hello World!");
+    }
+}
