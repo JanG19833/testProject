3f3bb0156a3ec7424ccdd501f86d969dee93ef22
jge <jan.gertgens@novatec-gmbh.de>
2016-01-15 11:19:55 +0100

    new Test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -11,18 +11,23 @@ public class HelloWorldTest {
     HelloWorld helloWorld;
 
     @Before
-    public void init(){
-        helloWorld=new HelloWorld();
+    public void init() {
+        helloWorld = new HelloWorld();
     }
 
     @Test
-    public void testString(){
-        Assert.assertEquals(helloWorld.testString(),"This is a test");
+    public void testString() {
+        Assert.assertEquals(helloWorld.testString(), "This is a test");
     }
+
     @Test
-    public void notEqualsString(){
+    public void notEqualsString() {
         Assert.assertNotEquals(helloWorld.testString(), "This is a tes");
     }
 
+    @Test
+    public void notEqualsString1() {
+        Assert.assertNotEquals(helloWorld.testString(), "This is a tes");
+    }
 
 }
